4b37fd58fa74fce34adf671b9f2a9246843b4008
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-01-09 23:55:29 +0100

    Initial Commit

---
+++ MKDataScanner/MKDataScanner.h
@@ -0,0 +1,13 @@
+//
+//  MKDataScanner.h
+//  MKDataScanner
+//
+//  Created by Marcin Krzyzanowski on 09/01/15.
+//  Copyright (c) 2015 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKDataScanner : NSObject
+
+@end
---
+++ MKDataScanner/MKDataScanner.m
@@ -0,0 +1,13 @@
+//
+//  MKDataScanner.m
+//  MKDataScanner
+//
+//  Created by Marcin Krzyzanowski on 09/01/15.
+//  Copyright (c) 2015 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import "MKDataScanner.h"
+
+@implementation MKDataScanner
+
+@end
